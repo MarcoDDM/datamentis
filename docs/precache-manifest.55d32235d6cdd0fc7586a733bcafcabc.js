60c1274532450b8a83819960ad26427fcd4e1835
MarcoDDM <marantal2013@hotmail.com>
2021-02-12 11:35:14 -0300

    react deploy

---
+++ docs/precache-manifest.55d32235d6cdd0fc7586a733bcafcabc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2466df7ff44554e90174",
+    "url": "/datamentis/static/css/main.9559fae8.chunk.css"
+  },
+  {
+    "revision": "2466df7ff44554e90174",
+    "url": "/datamentis/static/js/main.e948a5bd.chunk.js"
+  },
+  {
+    "revision": "5c8a7e0a58d4c181a044",
+    "url": "/datamentis/static/js/runtime~main.d45cbacd.js"
+  },
+  {
+    "revision": "1a9410e31d131aeedbbd",
+    "url": "/datamentis/static/css/2.16eb8d4f.chunk.css"
+  },
+  {
+    "revision": "1a9410e31d131aeedbbd",
+    "url": "/datamentis/static/js/2.b6e983c6.chunk.js"
+  },
+  {
+    "revision": "a173dc1bfc4d0204d2eb1ce7c61884e6",
+    "url": "/datamentis/static/media/Notaries.a173dc1b.svg"
+  },
+  {
+    "revision": "1f56d9a11e9a1afdf4548573427a4663",
+    "url": "/datamentis/index.html"
+  }
+];
